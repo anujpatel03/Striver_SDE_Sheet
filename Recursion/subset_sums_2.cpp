267f530d14c82dc9c9d176bb10b491f86bd9c48f
Anuj Patel <anujkpatel4@gmail.com>
2023-06-12 15:47:39 +0530

    Added method 2 of subset_sum_2

--- Recursion/subset_sums_2.cpp
+++ Recursion/subset_sums_2.cpp
@@ -1,6 +1,6 @@
-
-
 /*
+
+// Method 1
 
 class Solution {
 public:
@@ -27,6 +27,41 @@ public:
         vector<vector<int>>ans;
         for(auto it:st){
             ans.push_back(it);
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+// Method 2
+
+class Solution {
+public:
+
+    void subset(int i,vector<int>&v,vector<int>&arr,set<vector<int>>&st,int n){
+        if(i>=n){
+            // sort(v.begin(),v.end());
+            st.insert(v);
+            return;
+        }
+        v.push_back(arr[i]);
+        subset(i+1,v,arr,st,n);
+        v.pop_back();
+        subset(i+1,v,arr,st,n);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        set<vector<int>>st;
+        subset(0,v,nums,st,n);
+
+        for(auto it=st.begin();it!=st.end();it++){
+            ans.push_back(*it);
         }
         return ans;
     }
